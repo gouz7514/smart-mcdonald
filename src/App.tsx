742bf48dd70dc1175db0eadff04652c50f4fec78
gouz7514 <gouz7514@gmail.com>
2021-10-19 10:34:32 +0900

    Update : style, App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import './App.css';
+import './style/style.scss'
 
+import Header from './components/Header'
 import Button from './components/Button'
 
 function App() {
   return (
     <div className="App">
-      <Button textColor="white" text="hello world" className="test" name="Kim"></Button>
+      <Header title="This is title" />
+      <div className="container">
+        <Button textColor="white" text="hello world" className="test" name="Kim"></Button>
+      </div>
     </div>
   );
 }
